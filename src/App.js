32b591f93feaba6c22977568a0678388546c1a5f
Esha Patel <Patellesha11@gmail.com>
2025-05-19 11:56:35 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import react from "react";
+
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
 import Checkout from "./Pages/Checkout";
 import Cart from "./Pages/Cart";
-import ProductDetail from './Pages/ProductDetail';
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
       <Products />
       <Checkout />
