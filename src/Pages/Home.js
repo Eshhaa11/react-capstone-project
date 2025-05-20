8cfc3af6763568930ad41c896ec372c7bfd0a78c
Esha Patel <Patellesha11@gmail.com>
2025-05-20 10:47:38 +0300

    Added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,14 +4,12 @@ import '../Styles/Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="overlay">
         <div className="hero-content">
           <h1>Welcome to Shophoria 🛒</h1>
           <p>Find It. Love It. Get It.</p>
           <a href="/products">Shop Now</a>
         </div>
       </div>
-    </div>
   );
 };
 
