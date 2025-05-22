5176f0c3b7f4a5ec7f30c154059cab4152f4fdfe
Esha Patel <Patellesha11@gmail.com>
2025-05-22 12:12:25 +0300

    added

--- src/data/productdata.js
+++ src/data/productdata.js
@@ -21,7 +21,7 @@ import Image20 from '../Assets/Image20.jpg';
 import Image21 from '../Assets/Image21.jpg';
 import Image22 from '../Assets/Image22.jpg';
 import Image23 from '../Assets/Image23.jpg';
-import Image20 from '../Assets/Image20.jpg';
+import Image24 from '../Assets/Image24.jpg';
 import Image20 from '../Assets/Image20.jpg';
 import Image20 from '../Assets/Image20.jpg';
 import Image20 from '../Assets/Image20.jpg';
