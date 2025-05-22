a979e743e45a23d99545e61c7505f93523c37f7f
Esha Patel <Patellesha11@gmail.com>
2025-05-22 08:55:55 +0300

    added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,11 @@ import '../Styles/Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      {/* no img tag needed, bg-image is via CSS */}
+      <div className="overlay-content">
+        <h1>GrindFits</h1>
+        <p>Fit for the Hustle.</p>
+        <a href="/products">Shop Now</a>
+      </div>
     </div>
   );
 };
