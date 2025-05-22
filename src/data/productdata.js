3950963106cda4395ed24f72a85f6d73d0e0286c
Esha Patel <Patellesha11@gmail.com>
2025-05-22 11:25:25 +0300

    added

--- src/data/productdata.js
+++ src/data/productdata.js
@@ -17,7 +17,7 @@ import Image16 from '../Assets/Image16.jpg';
 import Image17 from '../Assets/Image17.jpg';
 import Image18 from '../Assets/Image18.jpg';
 import Image19 from '../Assets/Image19.jpg';
-import Image10 from '../Assets/Image10.jpg';
+import Image20 from '../Assets/Image20.jpg';
 
 const products = [
   // MEN
