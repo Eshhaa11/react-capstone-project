7f2df025232abce4b59c6e4db5be730b70a1cbdd
Esha Patel <Patellesha11@gmail.com>
2025-05-22 11:14:58 +0300

    Added

--- src/data/productdata.js
+++ src/data/productdata.js
@@ -5,7 +5,7 @@ import Image4 from '../Assets/Image4.jpg';
 import Image5 from '../Assets/Image5.jpg';
 import Image6 from '../Assets/Image6.jpg';
 import Image7 from '../Assets/Image7.jpg';
-import Image1 from '../Assets/Image1.jpg';
+import Image8 from '../Assets/Image8.jpg';
 import Image1 from '../Assets/Image1.jpg';
 import Image1 from '../Assets/Image1.jpg';
 
