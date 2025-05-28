6db00bd51a0ddc9ff2106dd2745246d6e06b62c3
Esha Patel <Patellesha11@gmail.com>
2025-05-28 11:06:25 +0300

    added

--- src/data/productdata.js
+++ src/data/productdata.js
@@ -1,63 +1,63 @@
-import Image1 from '../Assets/Image1.jpg';
-import Image2 from '../Assets/Image2.jpg';
-import Image3 from '../Assets/Image3.jpg';
-import Image4 from '../Assets/Image4.jpg';
-import Image5 from '../Assets/Image5.jpg';
-import Image6 from '../Assets/Image6.jpg';
-import Image7 from '../Assets/Image7.jpg';
-import Image8 from '../Assets/Image8.jpg';
-import Image9 from '../Assets/Image9.jpg';
-import Image10 from '../Assets/Image10.jpg';
-import Image11 from '../Assets/Image11.jpg';
-import Image12 from '../Assets/Image12.jpg';
-import Image13 from '../Assets/Image13.jpg';
-import Image14 from '../Assets/Image14.jpg';
-import Image15 from '../Assets/Image15.jpg';
-import Image16 from '../Assets/Image16.jpg';
-import Image17 from '../Assets/Image17.jpg';
-import Image18 from '../Assets/Image18.jpg';
-import Image19 from '../Assets/Image19.jpg';
-import Image20 from '../Assets/Image20.jpg';
-import Image21 from '../Assets/Image21.jpg';
-import Image22 from '../Assets/Image22.jpg';
-import Image23 from '../Assets/Image23.jpg';
-import Image24 from '../Assets/Image24.jpg';
-import Image25 from '../Assets/Image25.jpg';
-import Image26 from '../Assets/Image26.jpg';
-import Image27 from '../Assets/Image27.jpg';
-import Image28 from '../Assets/Image28.jpg';
-import Image29 from '../Assets/Image29.jpg';
-import Image30 from '../Assets/Image30.jpg';
-import Image31 from '../Assets/Image31.jpg';
-import Image32 from '../Assets/Image32.jpg';
-import Image33 from '../Assets/Image33.jpg';
-import Image34 from '../Assets/Image34.jpg';
-import Image35 from '../Assets/Image35.jpg';
-import Image36 from '../Assets/Image36.jpg';
-import Image37 from '../Assets/Image37.jpg';
-import Image38 from '../Assets/Image38.jpg';
-import Image39 from '../Assets/Image39.jpg';
-import Image40 from '../Assets/Image40.jpg';
-import Image41 from '../Assets/Image41.jpg';
-import Image42 from '../Assets/Image42.jpg';
-import Image43 from '../Assets/Image43.jpg';
-import Image44 from '../Assets/Image44.jpg';
-import Image45 from '../Assets/Image45.jpg';
-import Image46 from '../Assets/Image46.jpg';
-import Image47 from '../Assets/Image47.jpg';
-import Image48 from '../Assets/Image48.jpg';
-import Image49 from '../Assets/Image49.jpg';
-import Image50 from '../Assets/Image50.jpg';
-import Image51 from '../Assets/Image51.jpg';
-import Image52 from '../Assets/Image52.jpg';
-import Image53 from '../Assets/Image53.jpg';
-import Image54 from '../Assets/Image54.jpg';
-import Image55 from '../Assets/Image55.jpg';
-import Image56 from '../Assets/Image56.jpg';
-import Image57 from '../Assets/Image57.jpg';
-import Image58 from '../Assets/Image58.jpg';
-import Image59 from '../Assets/Image59.jpg';
-import Image60 from '../Assets/Image60.jpg';
+import Image1 from "../Assets/Image1.jpg";
+import Image2 from "../Assets/Image2.jpg";
+import Image3 from "../Assets/Image3.jpg";
+import Image4 from "../Assets/Image4.jpg";
+import Image5 from "../Assets/Image5.jpg";
+import Image6 from "../Assets/Image6.jpg";
+import Image7 from "../Assets/Image7.jpg";
+import Image8 from "../Assets/Image8.jpg";
+import Image9 from "../Assets/Image9.jpg";
+import Image10 from "../Assets/Image10.jpg";
+import Image11 from "../Assets/Image11.jpg";
+import Image12 from "../Assets/Image12.jpg";
+import Image13 from "../Assets/Image13.jpg";
+import Image14 from "../Assets/Image14.jpg";
+import Image15 from "../Assets/Image15.jpg";
+import Image16 from "../Assets/Image16.jpg";
+import Image17 from "../Assets/Image17.jpg";
+import Image18 from "../Assets/Image18.jpg";
+import Image19 from "../Assets/Image19.jpg";
+import Image20 from "../Assets/Image20.jpg";
+import Image21 from "../Assets/Image21.jpg";
+import Image22 from "../Assets/Image22.jpg";
+import Image23 from "../Assets/Image23.jpg";
+import Image24 from "../Assets/Image24.jpg";
+import Image25 from "../Assets/Image25.jpg";
+import Image26 from "../Assets/Image26.jpg";
+import Image27 from "../Assets/Image27.jpg";
+import Image28 from "../Assets/Image28.jpg";
+import Image29 from "../Assets/Image29.jpg";
+import Image30 from "../Assets/Image30.jpg";
+import Image31 from "../Assets/Image31.jpg";
+import Image32 from "../Assets/Image32.jpg";
+import Image33 from "../Assets/Image33.jpg";
+import Image34 from "../Assets/Image34.jpg";
+import Image35 from "../Assets/Image35.jpg";
+import Image36 from "../Assets/Image36.jpg";
+import Image37 from "../Assets/Image37.jpg";
+import Image38 from "../Assets/Image38.jpg";
+import Image39 from "../Assets/Image39.jpg";
+import Image40 from "../Assets/Image40.jpg";
+import Image41 from "../Assets/Image41.jpg";
+import Image42 from "../Assets/Image42.jpg";
+import Image43 from "../Assets/Image43.jpg";
+import Image44 from "../Assets/Image44.jpg";
+import Image45 from "../Assets/Image45.jpg";
+import Image46 from "../Assets/Image46.jpg";
+import Image47 from "../Assets/Image47.jpg";
+import Image48 from "../Assets/Image48.jpg";
+import Image49 from "../Assets/Image49.jpg";
+import Image50 from "../Assets/Image50.jpg";
+import Image51 from "../Assets/Image51.jpg";
+import Image52 from "../Assets/Image52.jpg";
+import Image53 from "../Assets/Image53.jpg";
+import Image54 from "../Assets/Image54.jpg";
+import Image55 from "../Assets/Image55.jpg";
+import Image56 from "../Assets/Image56.jpg";
+import Image57 from "../Assets/Image57.jpg";
+import Image58 from "../Assets/Image58.jpg";
+import Image59 from "../Assets/Image59.jpg";
+import Image60 from "../Assets/Image60.jpg";
 
 const products = [
   // MEN
@@ -67,7 +67,7 @@ const products = [
     title: "Men's Leather Jacket",
     price: 79.99,
     description: "Stylish and durable leather jacket.",
-    image: Image1
+    image: Image1,
   },
   {
     id: 2,
@@ -75,7 +75,7 @@ const products = [
     title: "Men's Casual Shirt",
     price: 24.99,
     description: "Comfortable cotton casual shirt.",
-    image: Image2
+    image: Image2,
   },
   {
     id: 3,
@@ -83,7 +83,7 @@ const products = [
     title: "Men's Denim Jeans",
     price: 49.99,
     description: "Classic fit denim jeans.",
-    image: Image3
+    image: Image3,
   },
   {
     id: 4,
@@ -91,7 +91,7 @@ const products = [
     title: "Men's Polo Shirt",
     price: 29.99,
     description: "Soft breathable polo shirt.",
-    image: Image4
+    image: Image4,
   },
   {
     id: 5,
@@ -99,7 +99,7 @@ const products = [
     title: "Men's Chino Pants",
     price: 39.99,
     description: "Slim fit chino pants.",
-    image: Image5
+    image: Image5,
   },
   {
     id: 6,
@@ -107,7 +107,7 @@ const products = [
     title: "Men's Sweatshirt",
     price: 34.99,
     description: "Cozy cotton sweatshirt.",
-    image: Image6
+    image: Image6,
   },
   {
     id: 7,
@@ -115,7 +115,7 @@ const products = [
     title: "Men's Tank Top",
     price: 14.99,
     description: "Lightweight summer tank top.",
-    image: Image7
+    image: Image7,
   },
   {
     id: 8,
@@ -123,7 +123,7 @@ const products = [
     title: "Men's Formal Suit",
     price: 149.99,
     description: "Elegant two-piece formal suit.",
-    image: Image8
+    image: Image8,
   },
   {
     id: 9,
@@ -131,7 +131,7 @@ const products = [
     title: "Men's Hoodie",
     price: 39.99,
     description: "Warm fleece hoodie.",
-    image: Image9
+    image: Image9,
   },
   {
     id: 10,
@@ -139,7 +139,7 @@ const products = [
     title: "Men's Shorts",
     price: 24.99,
     description: "Comfortable casual shorts.",
-    image: Image10
+    image: Image10,
   },
   {
     id: 11,
@@ -147,7 +147,7 @@ const products = [
     title: "Men's Windbreaker Jacket",
     price: 59.99,
     description: "Lightweight windbreaker for outdoors.",
-    image: Image11
+    image: Image11,
   },
   {
     id: 12,
@@ -155,7 +155,7 @@ const products = [
     title: "Men's Graphic Tee",
     price: 19.99,
     description: "Trendy graphic print T-shirt.",
-    image: Image12
+    image: Image12,
   },
   {
     id: 13,
@@ -163,7 +163,7 @@ const products = [
     title: "Men's Button-Down Shirt",
     price: 29.99,
     description: "Classic button-down shirt.",
-    image: Image13
+    image: Image13,
   },
   {
     id: 14,
@@ -171,7 +171,7 @@ const products = [
     title: "Men's Cargo Pants",
     price: 44.99,
     description: "Functional cargo pants.",
-    image: Image14
+    image: Image14,
   },
   {
     id: 15,
@@ -179,7 +179,7 @@ const products = [
     title: "Men's Tracksuit",
     price: 69.99,
     description: "Comfortable tracksuit for sports.",
-    image: Image15
+    image: Image15,
   },
   {
     id: 16,
@@ -187,7 +187,7 @@ const products = [
     title: "Men's Flannel Shirt",
     price: 34.99,
     description: "Warm and soft flannel shirt.",
-    image: Image16
+    image: Image16,
   },
   {
     id: 17,
@@ -195,7 +195,7 @@ const products = [
     title: "Men's Overcoat",
     price: 129.99,
     description: "Classic wool overcoat.",
-    image: Image17
+    image: Image17,
   },
   {
     id: 18,
@@ -203,7 +203,7 @@ const products = [
     title: "Men's Sweatpants",
     price: 29.99,
     description: "Relaxed fit sweatpants.",
-    image: Image18
+    image: Image18,
   },
   {
     id: 19,
@@ -211,7 +211,7 @@ const products = [
     title: "Men's Dress Shirt",
     price: 39.99,
     description: "Formal dress shirt.",
-    image: Image19
+    image: Image19,
   },
   {
     id: 20,
@@ -219,7 +219,7 @@ const products = [
     title: "Men's Winter Jacket",
     price: 99.99,
     description: "Insulated winter jacket.",
-    image: Image20
+    image: Image20,
   },
 
   // WOMEN
@@ -229,7 +229,7 @@ const products = [
     title: "Women's Summer Dress",
     price: 39.99,
     description: "Light and breezy summer dress.",
-    image: Image21
+    image: Image21,
   },
   {
     id: 22,
@@ -237,7 +237,7 @@ const products = [
     title: "Women's Blouse",
     price: 29.99,
     description: "Elegant and stylish blouse.",
-    image: Image22
+    image: Image22,
   },
   {
     id: 23,
@@ -245,7 +245,7 @@ const products = [
     title: "Women's Skinny Jeans",
     price: 49.99,
     description: "Classic skinny jeans.",
-    image:  Image23
+    image: Image23,
   },
   {
     id: 24,
@@ -253,7 +253,7 @@ const products = [
     title: "Women's Cardigan",
     price: 34.99,
     description: "Soft knitted cardigan.",
-    image:  Image24
+    image: Image24,
   },
   {
     id: 25,
@@ -261,7 +261,7 @@ const products = [
     title: "Women's Maxi Dress",
     price: 59.99,
     description: "Flowy maxi dress for special occasions.",
-    image:  Image25
+    image: Image25,
   },
   {
     id: 26,
@@ -269,7 +269,7 @@ const products = [
     title: "Women's Denim Jacket",
     price: 49.99,
     description: "Classic denim jacket.",
-    image:  Image26
+    image: Image26,
   },
   {
     id: 27,
@@ -277,7 +277,7 @@ const products = [
     title: "Women's T-Shirt",
     price: 19.99,
     description: "Casual cotton T-shirt.",
-    image:  Image27
+    image: Image27,
   },
   {
     id: 28,
@@ -285,7 +285,7 @@ const products = [
     title: "Women's Leggings",
     price: 24.99,
     description: "Comfortable stretch leggings.",
-    image:  Image28
+    image: Image28,
   },
   {
     id: 29,
@@ -293,7 +293,7 @@ const products = [
     title: "Women's Hoodie",
     price: 39.99,
     description: "Soft fleece hoodie.",
-    image:  Image29
+    image: Image29,
   },
   {
     id: 30,
@@ -301,7 +301,7 @@ const products = [
     title: "Women's Wrap Dress",
     price: 44.99,
     description: "Elegant wrap dress.",
-    image:  Image30
+    image: Image30,
   },
   {
     id: 31,
@@ -309,7 +309,7 @@ const products = [
     title: "Women's Shorts",
     price: 24.99,
     description: "Casual cotton shorts.",
-    image:  Image31
+    image: Image31,
   },
   {
     id: 32,
@@ -317,7 +317,7 @@ const products = [
     title: "Women's Sweatpants",
     price: 29.99,
     description: "Cozy sweatpants.",
-    image:  Image32
+    image: Image32,
   },
   {
     id: 33,
@@ -325,7 +325,7 @@ const products = [
     title: "Women's Tank Top",
     price: 14.99,
     description: "Light summer tank top.",
-    image:  Image33
+    image: Image33,
   },
   {
     id: 34,
@@ -333,7 +333,7 @@ const products = [
     title: "Women's Coat",
     price: 99.99,
     description: "Warm winter coat.",
-    image:  Image34
+    image: Image34,
   },
   {
     id: 35,
@@ -341,7 +341,7 @@ const products = [
     title: "Women's Midi Skirt",
     price: 34.99,
     description: "Elegant midi skirt.",
-    image:  Image35
+    image: Image35,
   },
   {
     id: 36,
@@ -349,7 +349,7 @@ const products = [
     title: "Women's Formal Blazer",
     price: 69.99,
     description: "Tailored formal blazer.",
-    image:  Image36 
+    image: Image36,
   },
   {
     id: 37,
@@ -357,7 +357,7 @@ const products = [
     title: "Women's Graphic Tee",
     price: 19.99,
     description: "Trendy graphic T-shirt.",
-    image:  Image37
+    image: Image37,
   },
   {
     id: 38,
@@ -365,7 +365,7 @@ const products = [
     title: "Women's Jeans Jacket",
     price: 49.99,
     description: "Casual jeans jacket.",
-    image:  Image38
+    image: Image38,
   },
   {
     id: 39,
@@ -373,7 +373,7 @@ const products = [
     title: "Women's Crop Top",
     price: 19.99,
     description: "Stylish crop top.",
-    image:  Image39
+    image: Image39,
   },
   {
     id: 40,
@@ -381,7 +381,7 @@ const products = [
     title: "Women's Sweater",
     price: 39.99,
     description: "Warm knit sweater.",
-    image: Image40
+    image: Image40,
   },
 
   // KIDS
@@ -391,7 +391,7 @@ const products = [
     title: "Kids' Hoodie",
     price: 29.99,
     description: "Soft and warm kids' hoodie.",
-    image: Image41
+    image: Image41,
   },
   {
     id: 42,
@@ -399,7 +399,7 @@ const products = [
     title: "Kids' T-Shirt",
     price: 14.99,
     description: "Bright and fun kids' T-shirt.",
-    image:  Image42
+    image: Image42,
   },
   {
     id: 43,
@@ -407,7 +407,7 @@ const products = [
     title: "Kids' Jeans",
     price: 29.99,
     description: "Durable kids' jeans.",
-    image:  Image43
+    image: Image43,
   },
   {
     id: 44,
@@ -415,7 +415,7 @@ const products = [
     title: "Kids' Shorts",
     price: 19.99,
     description: "Comfortable kids' shorts.",
-    image:  Image44
+    image: Image44,
   },
   {
     id: 45,
@@ -423,7 +423,7 @@ const products = [
     title: "Kids' Sweater",
     price: 24.99,
     description: "Warm kids' sweater.",
-    image:  Image45
+    image: Image45,
   },
   {
     id: 46,
@@ -431,7 +431,7 @@ const products = [
     title: "Kids' Jacket",
     price: 49.99,
     description: "Water-resistant kids' jacket.",
-    image:  Image46
+    image: Image46,
   },
   {
     id: 47,
@@ -439,7 +439,7 @@ const products = [
     title: "Kids' Dress",
     price: 34.99,
     description: "Cute kids' dress.",
-    image: Image47
+    image: Image47,
   },
   {
     id: 48,
@@ -447,7 +447,7 @@ const products = [
     title: "Kids' Pajamas",
     price: 19.99,
     description: "Soft cotton pajamas.",
-    image:  Image48
+    image: Image48,
   },
   {
     id: 49,
@@ -455,7 +455,7 @@ const products = [
     title: "Kids' Tank Top",
     price: 14.99,
     description: "Lightweight tank top.",
-    image:  Image49
+    image: Image49,
   },
   {
     id: 50,
@@ -463,7 +463,7 @@ const products = [
     title: "Kids' Raincoat",
     price: 29.99,
     description: "Bright and waterproof raincoat.",
-    image:  Image50
+    image: Image50,
   },
   {
     id: 51,
@@ -471,7 +471,7 @@ const products = [
     title: "Kids' Hoodie Jacket",
     price: 34.99,
     description: "Warm hoodie jacket.",
-    image:  Image51
+    image: Image51,
   },
   {
     id: 52,
@@ -479,7 +479,7 @@ const products = [
     title: "Kids' Sweatpants",
     price: 24.99,
     description: "Comfortable sweatpants.",
-    image:  Image52
+    image: Image52,
   },
   {
     id: 53,
@@ -487,7 +487,7 @@ const products = [
     title: "Kids' Cap",
     price: 9.99,
     description: "Fun kids' cap.",
-    image:  Image53
+    image: Image53,
   },
   {
     id: 54,
@@ -495,7 +495,7 @@ const products = [
     title: "Kids' Sneakers",
     price: 39.99,
     description: "Durable kids' sneakers.",
-    image:  Image54
+    image: Image54,
   },
   {
     id: 55,
@@ -503,7 +503,7 @@ const products = [
     title: "Kids' Gloves",
     price: 9.99,
     description: "Warm gloves for kids.",
-    image:  Image55
+    image: Image55,
   },
   {
     id: 56,
@@ -511,7 +511,7 @@ const products = [
     title: "Kids' Scarf",
     price: 14.99,
     description: "Colorful scarf.",
-    image:  Image56
+    image: Image56,
   },
   {
     id: 57,
@@ -519,7 +519,7 @@ const products = [
     title: "Kids' Winter Coat",
     price: 69.99,
     description: "Insulated winter coat.",
-    image:  Image57
+    image: Image57,
   },
   {
     id: 58,
@@ -527,7 +527,7 @@ const products = [
     title: "Kids' Snow Pants",
     price: 49.99,
     description: "Waterproof snow pants.",
-    image:  Image58
+    image: Image58,
   },
   {
     id: 59,
@@ -535,7 +535,7 @@ const products = [
     title: "Kids' Turtleneck",
     price: 19.99,
     description: "Cozy turtleneck sweater.",
-    image: Image59
+    image: Image59,
   },
   {
     id: 60,
@@ -543,8 +543,8 @@ const products = [
     title: "Kids' Overalls",
     price: 39.99,
     description: "Durable overalls for playtime.",
-    image: Image60
-  }
+    image: Image60,
+  },
 ];
 
 export default products;
