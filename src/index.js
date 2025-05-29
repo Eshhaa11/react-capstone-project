53249e2901b2bbaf13f28dcaf958a229516bcb69
Esha Patel <Patellesha11@gmail.com>
2025-05-29 10:55:40 +0300

    added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CartProvider } from './Context/CartContext.js';
-import { WishlistProvider } from './Context/WishlistContext.js'; // import WishlistProvider
+import { WishlistProvider } from './Context/WishlistContext.js'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
